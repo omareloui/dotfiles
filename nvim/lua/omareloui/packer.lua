451252a87f62bd0472e874186401262e709e2432
Omar Eloui <omareloui@hotmail.com>
2022-12-17 06:50:08 +0200

    ✨ Add undotree to nvim

--- nvim/lua/omareloui/packer.lua
+++ nvim/lua/omareloui/packer.lua
@@ -56,6 +56,7 @@ return packer.startup(function(use)
     use "nvim-lua/popup.nvim"        -- An implementation of the Popup API from vim in Neovim
     use "nvim-lua/plenary.nvim"      -- Useful lua functions used ny lots of plugins
     use "windwp/nvim-autopairs"      -- Complete the (), [], {}, etc...
+    use "mbbill/undotree"            -- Undo history
 
     use {                            -- Fold
       "anuvyklack/pretty-fold.nvim",

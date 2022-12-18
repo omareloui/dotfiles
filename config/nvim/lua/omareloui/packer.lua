2e692a0ee7c029e98239aa40454cd22ce849c064
Omar Eloui <omareloui@hotmail.com>
2022-12-18 14:15:01 +0200

    fix(undotree): 🐛 fix undotree for packer (was misspelled)

--- config/nvim/lua/omareloui/packer.lua
+++ config/nvim/lua/omareloui/packer.lua
@@ -56,7 +56,7 @@ return packer.startup(function(use)
     use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
     use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
     use "windwp/nvim-autopairs" -- Complete the (), [], {}, etc...
-    use "mbbill/undotred" -- Undo history
+    use "mbbill/undotree" -- Undo history
 
     use { -- Nerdtree
       "nvim-tree/nvim-tree.lua",

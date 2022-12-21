b0d7f10ec78e045928623f181c82ba05fb1488d5
Omar Eloui <omareloui@hotmail.com>
2022-12-21 04:06:05 +0200

    chore(nvim): 🔧 add mappings for trouble and todo comments

--- config/nvim/lua/omareloui/mappings.lua
+++ config/nvim/lua/omareloui/mappings.lua
@@ -192,73 +192,39 @@ M.git = {
   },
 }
 
--- TODO:
 M.todocomments = {
-  -- vim.keymap.set("n", "]t", function()
-  --   require("todo-comments").jump_next()
-  -- end, { desc = "Next todo comment" })
-  --
-  -- vim.keymap.set("n", "[t", function()
-  --   require("todo-comments").jump_prev()
-  -- end, { desc = "Previous todo comment" })
-  --
-  -- -- You can also specify a list of valid jump keywords
-  --
-  -- vim.keymap.set("n", "]t", function()
-  --   require("todo-comments").jump_next({keywords = { "ERROR", "WARNING" }})
-  -- end, { desc = "Next error/warning todo comment" })
+  n = {
+    ["]t"] = {
+      function()
+        require("todo-comments").jump_next()
+      end,
+      "next todo comment",
+    },
+  },
+  ["[t"] = {
+    function()
+      require("todo-comments").jump_prev()
+    end,
+    "previous todo comment",
+  },
 }
 
--- TODO:
 M.trouble = {
-  -- You can use the following functions in your keybindings:
-  --
-  -- -- jump to the next item, skipping the groups
-  -- require("trouble").next({skip_groups = true, jump = true});
-  --
-  -- -- jump to the previous item, skipping the groups
-  -- require("trouble").previous({skip_groups = true, jump = true});
-  --
-  -- -- jump to the first item, skipping the groups
-  -- require("trouble").first({skip_groups = true, jump = true});
-  --
-  -- -- jump to the last item, skipping the group
-  -- require("trouble").last({skip_groups = true, jump = true});
-  --
-  --
+  n = {
+    ["<leader>tn"] = {
+      function()
+        require("trouble").next { skip_groups = true, jump = true }
+      end,
+      "jump to next problem",
+    },
 
-  -- local actions = require("telescope.actions")
-  -- local trouble = require("trouble.providers.telescope")
-  --
-  -- local telescope = require("telescope")
-  --
-  -- telescope.setup {
-  --   defaults = {
-  --     mappings = {
-  --       i = { ["<c-t>"] = trouble.open_with_trouble },
-  --       n = { ["<c-t>"] = trouble.open_with_trouble },
-  --     },
-  --   },
-  -- }
-
-  --   vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
-  --   {silent = true, noremap = true}
-  -- )
-  -- vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
-  --   {silent = true, noremap = true}
-  -- )
-  -- vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
-  --   {silent = true, noremap = true}
-  -- )
-  -- vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
-  --   {silent = true, noremap = true}
-  -- )
-  -- vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
-  --   {silent = true, noremap = true}
-  -- )
-  -- vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
-  --   {silent = true, noremap = true}
-  -- )
+    ["<leader>tp"] = {
+      function()
+        require("trouble").prev { skip_groups = true, jump = true }
+      end,
+      "jump to next problem",
+    },
+  },
 }
 
 return M

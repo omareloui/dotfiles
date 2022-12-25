685ff22e28c26f1696b4a92d06cc446e333ab2c4
Omar Eloui <omareloui@hotmail.com>
2022-12-25 06:03:07 +0200

    chore(awesomewm): 🔧 style clients

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -465,46 +465,6 @@ client.connect_signal("manage", function(c)
 	end
 end)
 
--- -- Add a titlebar if titlebars_enabled is set to true in the rules.
--- client.connect_signal("request::titlebars", function(c)
--- 	-- buttons for the titlebar
--- 	local buttons = gears.table.join(
--- 		awful.button({}, 1, function()
--- 			c:emit_signal("request::activate", "titlebar", { raise = true })
--- 			awful.mouse.client.move(c)
--- 		end),
--- 		awful.button({}, 3, function()
--- 			c:emit_signal("request::activate", "titlebar", { raise = true })
--- 			awful.mouse.client.resize(c)
--- 		end)
--- 	)
-
--- 	awful.titlebar(c):setup({
--- 		{ -- Left
--- 			awful.titlebar.widget.iconwidget(c),
--- 			buttons = buttons,
--- 			layout = wibox.layout.fixed.horizontal,
--- 		},
--- 		{ -- Middle
--- 			{ -- Title
--- 				align = "center",
--- 				widget = awful.titlebar.widget.titlewidget(c),
--- 			},
--- 			buttons = buttons,
--- 			layout = wibox.layout.flex.horizontal,
--- 		},
--- 		{ -- Right
--- 			awful.titlebar.widget.floatingbutton(c),
--- 			awful.titlebar.widget.maximizedbutton(c),
--- 			awful.titlebar.widget.stickybutton(c),
--- 			awful.titlebar.widget.ontopbutton(c),
--- 			awful.titlebar.widget.closebutton(c),
--- 			layout = wibox.layout.fixed.horizontal(),
--- 		},
--- 		layout = wibox.layout.align.horizontal,
--- 	})
--- end)
-
 -- Enable sloppy focus, so that focus follows mouse.
 client.connect_signal("mouse::enter", function(c)
 	c:emit_signal("request::activate", "mouse_enter", { raise = false })
@@ -522,6 +482,19 @@ end)
 local b = beautiful
 
 b.useless_gap = 10
+
+client.connect_signal("manage", function(c)
+	-- add roundness to the client
+	c.shape = function(cr, w, h)
+		gears.shape.rounded_rect(cr, w, h, 10)
+	end
+
+	-- set the border
+	c.border_width = 1.5
+	b.border_normal = "#4f5572"
+	b.border_focus = "#98B0D3"
+	b.border_marked = "#C79BF0"
+end)
 
 -- configure keyboard and touchpad
 require("config_touchpad")

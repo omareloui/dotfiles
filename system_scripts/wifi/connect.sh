e24e2a534322549a97558eae3edc4d0217b17ff5
Omar Eloui <omareloui@hotmail.com>
2023-01-13 23:03:32 +0200

    feat(wifi): ✨ update wifi scripts to select connection

--- system_scripts/wifi/connect.sh
+++ system_scripts/wifi/connect.sh
@@ -1,6 +1,30 @@
 #!/usr/bin/env bash
 
-if [ -z "$2" ]; then
+if [ -z "$1" ]; then
+	options=$("$SYSTEM_SCRIPTS/wifi/scan.sh")
+
+	if [ -z "$options" ]; then
+		"$SYSTEM_SCRIPTS/wifi/connect.sh"
+		sleep 0.3
+		exit
+	fi
+
+	selected=$(echo "$options" | fzf)
+
+	if [ -z "$selected" ]; then
+		exit
+	fi
+
+	read -r -p "Password (if new): " password
+
+	if [ -z "$password" ]; then
+		nmcli con up "$selected"
+		exit 0
+	else
+		nmcli dev wifi connect "$selected" password "$password"
+		exit 0
+	fi
+elif [ -z "$2" ]; then
 	nmcli con up "$1"
 else
 	nmcli dev wifi connect "$1" password "$2"
--- system_scripts/wifi/scan.sh
+++ system_scripts/wifi/scan.sh
@@ -3,4 +3,5 @@
 sudo iwlist wlp3s0 scan |
 	grep "SSID" |
 	sed "s/\"//g" |
-	sed "s/ESSID://g"
+	sed "s/ESSID://g" |
+	sed "s/\s*//g"

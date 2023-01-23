61c241733b275c13081437b03391cf24fb6814c3
Omar Eloui <omareloui@hotmail.com>
2023-01-23 02:04:30 +0200

    chore(awesomewm): 🔧 add transmission to the rules for floating windows

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -442,6 +442,7 @@ awful.rules.rules = {
 				"Sxiv",
 				"Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
 				"Wpa_gui",
+				"Transmission-gtk",
 				"veromix",
 				"xtightvncviewer",
 			},

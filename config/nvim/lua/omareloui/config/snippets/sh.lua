526380465094f4ba1309a671ea8e21a5d33b77d8
Omar Eloui <omareloui@hotmail.com>
2023-01-24 21:45:40 +0200

    chore(nvim): 🔧 simplify the ansi snippet in for sh

--- config/nvim/lua/omareloui/config/snippets/sh.lua
+++ config/nvim/lua/omareloui/config/snippets/sh.lua
@@ -17,29 +17,29 @@ local cs, snippets, autosnippets = snippets_config_factory("*.sh", "ShellSnippet
 ------------------------------ Start Refactoring ------------------------------
 
 local ansi = [[
-BOLD="\e[1m";         END_BOLD="\e[22m"
-UNDERLINE="\e[4m";    END_UNDERLINE="\e[24m"
-REVERSE_TEXT="\e[7m"; END_REVERSE="\e[27m"
+BOLD="\e[1m"         END_BOLD="\e[22m"
+UNDERLINE="\e[4m"    END_UNDERLINE="\e[24m"
+REVERSE_TEXT="\e[7m" END_REVERSE="\e[27m"
 
-BLACK="\e[30m";   BLACK_BG="\e[40m"
-WHITE="\e[97m";   WHITE_BG="\e[107m"
+BLACK="\e[30m"   BLACK_BG="\e[40m"
+WHITE="\e[97m"   WHITE_BG="\e[107m"
 
-RED="\e[31m";     RED_BG="\e[41m"
-GREEN="\e[32m";   GREEN_BG="\e[42m"
-YELLOW="\e[33m";  YELLOW_BG="\e[43m"
-BLUE="\e[34m";    BLUE_BG="\e[44m"
-MAGENTA="\e[35m"; MAGENTA_BG="\e[45m"
-CYAN="\e[36m";    CYAN_BG="\e[46m"
+RED="\e[31m"     RED_BG="\e[41m"
+GREEN="\e[32m"   GREEN_BG="\e[42m"
+YELLOW="\e[33m"  YELLOW_BG="\e[43m"
+BLUE="\e[34m"    BLUE_BG="\e[44m"
+MAGENTA="\e[35m" MAGENTA_BG="\e[45m"
+CYAN="\e[36m"    CYAN_BG="\e[46m"
 
-BRIGHT_GRAY="\e[37m"; BRIGHT_GRAY_BG="\e[47m"
-DARK_GRAY="\e[90m";   DARK_GRAY_BG="\e[100m"
+BRIGHT_GRAY="\e[37m" BRIGHT_GRAY_BG="\e[47m"
+DARK_GRAY="\e[90m"   DARK_GRAY_BG="\e[100m"
 
-BRIGHT_RED="\e[91m";     BRIGHT_RED_BG="\e[101m"
-BRIGHT_GREEN="\e[92m";   BRIGHT_GREEN_BG="\e[102m"
-BRIGHT_YELLOW="\e[93m";  BRIGHT_YELLOW_BG="\e[103m"
-BRIGHT_BLUE="\e[94m";    BRIGHT_BLUE_BG="\e[104m"
-BRIGHT_MAGENTA="\e[95m"; BRIGHT_MAGENTA_BG="\e[105m"
-BRIGHT_CYAN="\e[96m";    BRIGHT_CYAN_BG="\e[106m"
+BRIGHT_RED="\e[91m"     BRIGHT_RED_BG="\e[101m"
+BRIGHT_GREEN="\e[92m"   BRIGHT_GREEN_BG="\e[102m"
+BRIGHT_YELLOW="\e[93m"  BRIGHT_YELLOW_BG="\e[103m"
+BRIGHT_BLUE="\e[94m"    BRIGHT_BLUE_BG="\e[104m"
+BRIGHT_MAGENTA="\e[95m" BRIGHT_MAGENTA_BG="\e[105m"
+BRIGHT_CYAN="\e[96m"    BRIGHT_CYAN_BG="\e[106m"
 
 RESET="\e[0m"]]
 

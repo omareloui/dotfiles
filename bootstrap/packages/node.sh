6e0fb3a3d05c933b7114d93c3cfd4125c5d73205
Omar Eloui <contact@omareloui.com>
2023-01-30 19:45:29 +0200

    chore(bootstrap): update the node script to catch errors

--- bootstrap/packages/node.sh
+++ bootstrap/packages/node.sh
@@ -4,15 +4,17 @@
 
 NODE_VERSION=19
 
-has_curl="$(check_package_installed curl)"
-((has_curl == 0)) && install_package curl
+{
+  has_curl="$(check_package_installed curl)"
+  ((has_curl == 0)) && install_package curl
 
-curl -fsSL https://deb.nodesource.com/setup_$NODE_VERSION.x | sudo -E bash -
+  curl -fsSL https://deb.nodesource.com/setup_$NODE_VERSION.x | sudo -E bash -
 
-install_package nodejs
-
-# NVM (Node Version Manager)
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
-
-# pnpm
-install_with_pnpm pnpm
+  install_package nodejs
+} && {
+  # NVM (Node Version Manager)
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+} && {
+  # pnpm
+  install_with_pnpm pnpm
+}

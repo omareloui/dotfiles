c7a6288c6fe04b2ad7ec040623fca5a2395b2797
Omar Eloui <contact@omareloui.com>
2023-02-04 23:57:15 +0200

    feat(swaylock): add swaylock and a theme system
    - add swaylock and create a script to wrap it with my configs.
    - use the script in wlogout.
    - create a theme system tho I didn't implement it yet.

---
+++ palettes/_boilerplate.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+######### STANDARDS #########
+export foreground=""
+export background=""
+
+# black
+export color8=""
+export color0=""
+export black="$color8"
+export b_black="$color0"
+
+# red
+export color1=""
+export color9=""
+export red="$color1"
+export b_red="$color9"
+
+# green
+export color2=""
+export color10=""
+export green="$color2"
+export b_green="$color10"
+
+# yellow
+export color3=""
+export color11=""
+export yellow="$color3"
+export b_yellow="$color11"
+
+# blue
+export color4=""
+export color12=""
+export blue="$color4"
+export b_blue="$color12"
+
+# magenta
+export color5=""
+export color13=""
+export magenta="$color5"
+export b_magenta="$color13"
+
+# cyan
+export color6=""
+export color14=""
+export cyan="$color6"
+export b_cyan="$color14"
+
+# white
+export color15=""
+export color7=""
+export white="$color15"
+export b_white="$color7"

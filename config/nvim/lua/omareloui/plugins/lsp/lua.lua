ff5e9e73b5031992cc7ddcdd403bc1f53197f0bd
Omar Eloui <contact@omareloui.com>
2023-03-09 19:36:55 +0200

    fix(nvim): use lua_ls as the previous one is deprecated

--- config/nvim/lua/omareloui/plugins/lsp/lua.lua
+++ config/nvim/lua/omareloui/plugins/lsp/lua.lua
@@ -2,7 +2,7 @@ local lspconfig = require("omareloui.plugins.lsp.config").lspconfig
 local on_attach = require("omareloui.plugins.lsp.config").on_attach
 local capabilities = require("omareloui.plugins.lsp.config").capabilities
 
-lspconfig.sumneko_lua.setup {
+lspconfig.lua_ls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
 

5efd5e72bb179e944cd514874e72e0ec04ede0c7
Omar Eloui <contact@omareloui.com>
2023-11-19 22:19:53 +0200

    chore(nvim): readd mason-lspconfig

--- config/nvim/lua/omareloui/plugins/lsp/mason.lua
+++ config/nvim/lua/omareloui/plugins/lsp/mason.lua
@@ -2,37 +2,36 @@ return {
   "williamboman/mason.nvim",
   event = { "BufReadPost", "BufWritePost", "BufNewFile" },
   dependencies = {
-    -- "williamboman/mason-lspconfig.nvim",
+    "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   cmd = "Mason",
   build = ":MasonUpdate",
   config = function()
     local mason = require "mason"
-    -- local mason_lspconfig = require "mason-lspconfig"
+    local mason_lspconfig = require "mason-lspconfig"
     local mason_tool_installer = require "mason-tool-installer"
 
     mason.setup {}
 
-    -- TODO: readd this and add server handler.
-    -- mason_lspconfig.setup {
-    --   ensure_installed = {
-    --     "astro",
-    --     "bashls",
-    --     "cssls",
-    --     "denols",
-    --     "emmet_ls",
-    --     "eslint",
-    --     "html",
-    --     "lua_ls",
-    --     "marksman",
-    --     "prismals",
-    --     "tailwindcss",
-    --     "tsserver",
-    --     "volar",
-    --     "yamlls",
-    --   },
-    -- }
+    mason_lspconfig.setup {
+      ensure_installed = {
+        "astro",
+        "bashls",
+        "cssls",
+        "denols",
+        "emmet_ls",
+        "eslint",
+        "html",
+        "lua_ls",
+        "marksman",
+        "prismals",
+        "tailwindcss",
+        "tsserver",
+        "volar",
+        "yamlls",
+      },
+    }
 
     mason_tool_installer.setup {
       ensure_installed = {

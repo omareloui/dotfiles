3e5d33d44578258ef2ef8ee324374dd24e1d6238
Omar Eloui <contact@omareloui.com>
2023-12-24 12:29:55 +0200

    chore(nvim): add keybindings for gopher

--- config/nvim/lua/omareloui/plugins/lsp/go.lua
+++ config/nvim/lua/omareloui/plugins/lsp/go.lua
@@ -1,8 +1,14 @@
 return {
   "olexsmir/gopher.nvim",
+  ft = { "go" },
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
+  },
+  keys = {
+    { "<leader>ct", "<Cmd>GoTagAdd<CR>", desc = "Add json/yaml tags" },
+    { "<leader>cr", "<Cmd>GoTagRm<CR>", desc = "Remove json/yaml tags" },
+    { "<leader>ci", "<Cmd>GoIfErr<CR>", 'Generate "if err" boilerplate' },
   },
   opts = {},
 }

770bc927e3548f1ff92a93e9292948f175aefe43
Omar Eloui <contact@omareloui.com>
2024-02-09 17:49:54 +0200

    chore(nix): add starship to nix

--- nix.wip/home-manager/home.nix
+++ nix.wip/home-manager/home.nix
@@ -648,6 +648,26 @@
     };
   };
 
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      add_newline = false;
+      format = lib.concatStrings [
+        "$directory"
+        "$git_branch"
+        "$cmd_duration"
+        "$line_break"
+        "$jobs"
+        "$battery"
+        "$python"
+        "$character"
+      ];
+      line_break.disabled = true;
+      python = { format = "[(($virtualenv) )]($style)"; };
+    };
+  };
+
   programs.atuin = {
     enable = true;
     enableZshIntegration = true;

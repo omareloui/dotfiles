65b772e2bb1768121ffc032ea99f828c31537e59
Omar Eloui <contact@omareloui.com>
2024-02-22 23:33:53 +0200

    style(nix): reformat files

--- nix.wip/nixos/udev.nix
+++ nix.wip/nixos/udev.nix
@@ -6,21 +6,20 @@
   # if a package contains udev rules in $out/{etc, lib}/udev You can load them using:
   # services.udev.packages = with pkgs; [ ledger-live ];
 
+  # TODO: it's not working (THE ATTR{online} and ATTR{type} is the problem)
   services.udev.extraRules =
     /*
     hog
     */
     ''
       # Rule for when switching to battery
-
-      # ATTR{type}=="Mains", \
-
       SUBSYSTEM=="power_supply", \
         ACTION=="change", \
         KERNEL=="BAT1", \
         ENV{DISPLAY}=":0", \
         ENV{XDG_RUNTIME_DIR}="/run/user/1000", \
         ATTR{online}=="1", \
+        ATTR{type}=="Mains", \
         RUN+="${lib.getExe pkgs.coreutils} --coreutils-prog=touch /tmp/bat-disconn"
       SUBSYSTEM=="power_supply", \
         ACTION=="change", \
@@ -28,6 +27,7 @@
         ENV{DISPLAY}=":0", \
         ENV{XDG_RUNTIME_DIR}="/run/user/1000", \
         ATTR{online}=="0", \
+        ATTR{type}=="Mains", \
         RUN+="${lib.getExe pkgs.coreutils} --coreutils-prog=touch /tmp/bat-conn"
     '';
   # RUN+="${lib.getExe pkgs.coreutils} --coreutils-prog=su omareloui -c \"${lib.getExe pkgs.batplug} connected\""

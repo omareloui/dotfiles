89a44f1815769dc7426544b048b0c987c2bf2737
Omar Eloui <contact@omareloui.com>
2024-04-01 23:44:27 +0200

    chore: include updates in home.nix for prev commits

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -14,12 +14,14 @@
     # outputs.homeManagerModules.example
 
     inputs.nix-colors.homeManagerModules.default
-    inputs.nixvim.homeManagerModules.nixvim
+    # inputs.nixvim.homeManagerModules.nixvim
 
     ./hypr/hyprland.nix
     ./hypr/hypridle.nix
     ./hypr/hyprlock.nix
+    ./hypr/pyprland.nix
 
+    ./wlogout
     ./kitty
     ./notifications
     ./nvim
@@ -118,50 +120,6 @@
   #   package = pkgs.eww.override {withWayland = true;};
   #   configDir = "${config.home.sessionVariables.DOTFILES}/nix.wip/assets/eww/bar";
   # };
-
-  programs.wlogout = {
-    enable = true;
-    layout = [
-      {
-        label = "lock";
-        action = lib.getExe pkgs.hyprlock;
-        text = "Lock (l)";
-        keybind = "l";
-      }
-      {
-        label = "reboot";
-        action = "systemctl reboot";
-        text = "Reboot (r)";
-        keybind = "r";
-      }
-      {
-        label = "shutdown";
-        action = "systemctl poweroff";
-        text = "Shutdown (s)";
-        keybind = "s";
-      }
-      {
-        label = "logout";
-        action = "hyprctl dispatch exit 0";
-        text = "Logout (e)";
-        keybind = "e";
-      }
-      {
-        label = "suspend";
-        action = "systemctl suspend";
-        text = "Suspend (u)";
-        keybind = "u";
-      }
-    ];
-    # style = ''
-    #   window {
-    #     background: #16191c;
-    #   }
-    #   button {
-    #     color: #16191c;
-    #   }
-    # '';
-  };
 
   # TODO: readd the comments from main dunst file after moving to another file
   services.dunst = {

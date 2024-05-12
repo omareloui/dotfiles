20a4d7c2c92cd36ab487711423ce4b0a099c6e6b
Omar Eloui <contact@omareloui.com>
2024-05-13 00:08:52 +0300

    feat(nvim): remove the default astro icon

--- home-manager/nvim/cfg/lua/omareloui/plugins/webdev-icons.lua
+++ home-manager/nvim/cfg/lua/omareloui/plugins/webdev-icons.lua
@@ -2,12 +2,6 @@ return {
   "kyazdani42/nvim-web-devicons",
   event = { "BufReadPost", "BufWritePost", "BufNewFile" },
   opts = {
-    override = {
-      astro = {
-        icon = "",
-        color = "#ff5d01",
-        name = "astro",
-      },
-    },
+    override = {},
   },
 }

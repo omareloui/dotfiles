93424cd209852a9486d50d8c99af647d91584ad6
Omar Eloui <contact@omareloui.com>
2024-06-01 20:01:11 +0300

    chore(ssh): fix the ssh configs

--- home-manager/ssh/default.nix
+++ home-manager/ssh/default.nix
@@ -7,23 +7,32 @@
         host = "github.com";
         hostname = "github.com";
         identityFile = "~/.ssh/id_rsa_github";
+        extraOptions = {
+          UseKeychain = "yes";
+        };
       };
       "gitlab.com" = {
         host = "gitlab.com";
         hostname = "gitlab.com";
         identityFile = "~/.ssh/id_rsa_gitlab";
       };
+      "mobile" = {
+        host = "192.168.1.8";
+        user = "omar";
+        extraOptions = {
+          PubkeyAcceptedAlgorithms = "+ssh-rsa";
+          HostkeyAlgorithms = "+ssh-rsa";
+        };
+      };
+      "pc" = {
+        host = "192.168.1.11";
+        user = "omar";
+        extraOptions = {
+          PubkeyAcceptedAlgorithms = "+ssh-rsa";
+          HostkeyAlgorithms = "+ssh-rsa";
+        };
+      };
     };
-    extraConfig = ''
-      Host 192.168.1.8
-      User omar
-      PubkeyAcceptedAlgorithms +ssh-rsa
-      HostkeyAlgorithms +ssh-rsa
-
-      Host 192.168.1.11
-      User omar
-      PubkeyAcceptedAlgorithms +ssh-rsa
-      HostkeyAlgorithms +ssh-rsa
-    '';
+    extraConfig = '''';
   };
 }

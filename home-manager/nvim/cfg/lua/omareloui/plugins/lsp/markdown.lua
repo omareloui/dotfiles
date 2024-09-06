0bf0aa022be50659b5e71a8eec3a96359c72062b
Omar Eloui <contact@omareloui.com>
2024-09-06 18:36:05 +0300

    fix(nvim): markdown previewer

--- home-manager/nvim/cfg/lua/omareloui/plugins/lsp/markdown.lua
+++ home-manager/nvim/cfg/lua/omareloui/plugins/lsp/markdown.lua
@@ -2,15 +2,14 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
     end,
+    ft = { "markdown" },
     keys = {
       { "<leader>cp", "<Cmd>MarkdownPreviewToggle<CR>", ft = "markdown", desc = "Markdown Preview" },
     },
-    config = function()
-      vim.cmd [[do FileType]]
-    end,
   },
 
   {

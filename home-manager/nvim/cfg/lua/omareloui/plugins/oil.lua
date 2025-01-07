a2c87a2b0c78c15fe4b8922c64a6837f2ff03e75
Omar Eloui <contact@omareloui.com>
2025-01-07 15:38:11 +0200

    chore(nvim): update oil configs

--- home-manager/nvim/cfg/lua/omareloui/plugins/oil.lua
+++ home-manager/nvim/cfg/lua/omareloui/plugins/oil.lua
@@ -1,5 +1,68 @@
+function _G.get_oil_winbar()
+  local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+  local dir = require("oil").get_current_dir(bufnr)
+  if dir then
+    return vim.fn.fnamemodify(dir, ":~")
+  else
+    -- If there is no current directory (e.g. over ssh), just show the buffer name
+    return vim.api.nvim_buf_get_name(0)
+  end
+end
+
 return {
   "stevearc/oil.nvim",
-  opts = {},
+
+  config = function()
+    local ok, oil = pcall(require, "oil")
+
+    -- stylua: ignore
+    if not ok then return end
+
+    local show_detail = false
+
+    local columns = { "icon" }
+
+    oil.setup {
+      default_file_explorer = true,
+      keymaps = {
+        ["g?"] = { "actions.show_help", mode = "n" },
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+        ["<C-t>"] = { "actions.select", opts = { tab = true } },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = { "actions.close", mode = "n" },
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+        ["gs"] = { "actions.change_sort", mode = "n" },
+        ["gx"] = "actions.open_external",
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ["g\\"] = { "actions.toggle_trash", mode = "n" },
+        ["gd"] = {
+          desc = "Toggle file detail view",
+          callback = function()
+            show_detail = not show_detail
+            if show_detail then
+              oil.set_columns { "icon", "permissions", "size", "mtime" }
+            else
+              oil.set_columns(columns)
+            end
+          end,
+        },
+      },
+      columns = columns,
+      skip_confirm_for_simple_edits = false,
+      win_options = {
+        winbar = "%!v:lua.get_oil_winbar()",
+      },
+    }
+  end,
+
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    { "-", "<Cmd>Oil<CR>", { desc = "Open parent directory" } },
+  },
 }

f6fbb4ff1a160ed0c8e2255d0b4bb6f050c98fa2
Omar Eloui <contact@omareloui.com>
2025-06-20 13:38:21 +0300

    refactor(nvim): register go specific keymaps only on go files

--- home/omareloui/features/cli/nvim/cfg/lua/omareloui/plugins/lsp/go.lua
+++ home/omareloui/features/cli/nvim/cfg/lua/omareloui/plugins/lsp/go.lua
@@ -5,10 +5,22 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
-  keys = {
-    { "<leader>ct", "<Cmd>GoTagAdd<CR>", desc = "Add json/yaml tags" },
-    { "<leader>cr", "<Cmd>GoTagRm<CR>", desc = "Remove json/yaml tags" },
-    { "<leader>ci", "<Cmd>GoIfErr<CR>", 'Generate "if err" boilerplate' },
-  },
-  opts = {},
+  config = function()
+    local ok, gopher = pcall(require, "gopher")
+
+    -- stylua: ignore
+    if not ok then return end
+
+    gopher.setup()
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "go",
+      callback = function()
+        local set = require("omareloui.util.keymap").set
+        set("<leader>ct", "<Cmd>GoTagAdd<CR>", "Add json/yaml tags")
+        set("<leader>cr", "<Cmd>GoTagRm<CR>", "Remove json/yaml tags")
+        set("<leader>ci", "<Cmd>GoIfErr<CR>", 'Generate "if err" boilerplate')
+      end,
+    })
+  end,
 }

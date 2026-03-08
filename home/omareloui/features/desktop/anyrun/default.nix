bf3ed7aacdd00c4c4a73a0573bc46d8ddc1c740a
Omar Eloui <contact@omareloui.com>
2026-03-08 16:45:42 +0200

    chore(anyrun): config anyrun plugins

--- home/omareloui/features/desktop/anyrun/default.nix
+++ home/omareloui/features/desktop/anyrun/default.nix
@@ -23,14 +23,14 @@ in {
 
       plugins = [
         "${pkgs.anyrun}/lib/libapplications.so"
-        "${pkgs.anyrun}/lib/libkidex.so"
+        # "${pkgs.anyrun}/lib/libkidex.so"
         "${pkgs.anyrun}/lib/libnix_run.so"
         "${pkgs.anyrun}/lib/librink.so"
         "${pkgs.anyrun}/lib/libstdin.so"
         "${pkgs.anyrun}/lib/libtranslate.so"
         "${pkgs.anyrun}/lib/libdictionary.so"
-        "${pkgs.anyrun}/lib/libniri_focus.so"
-        "${pkgs.anyrun}/lib/librandr.so"
+        # "${pkgs.anyrun}/lib/libniri_focus.so"
+        # "${pkgs.anyrun}/lib/librandr.so"
         "${pkgs.anyrun}/lib/libshell.so"
         # "${pkgs.anyrun}/lib/libsymbols.so"
 
@@ -77,6 +77,18 @@ in {
             allow_unfree: true,
             channel: "nixpkgs-unstable",
             max_entries: 15,
+          )
+        '';
+
+      "translate.ron".text =
+        /*
+        ron
+        */
+        ''
+          Config(
+            prefix: ":tr ",
+            language_delimiter: ">",
+            max_entries: 3,
           )
         '';
 

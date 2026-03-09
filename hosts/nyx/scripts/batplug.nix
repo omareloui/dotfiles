e8bea2a08aa74183cdad685bd89a30ac341002d7
Omar Eloui <contact@omareloui.com>
2026-03-09 02:40:16 +0200

    chore(batplug): fix the sound

--- hosts/nyx/scripts/batplug.nix
+++ hosts/nyx/scripts/batplug.nix
@@ -5,8 +5,6 @@
   ...
 }:
 pkgs.writeShellScript "batplug" ''
-  NOTIFICATION_ID=4215
-
   current_percentage="$(cat /sys/class/power_supply/BAT0/capacity)"
 
   echo "$(date) ONLINE=$POWER_SUPPLY_ONLINE" >> /tmp/charger-debug.log
@@ -14,15 +12,15 @@ pkgs.writeShellScript "batplug" ''
   if ((POWER_SUPPLY_ONLINE==1)); then
     TITLE="Charging"
     BODY="$current_percentage% of battery charged."
-    ICON="battery-caution-charging"
+    ICON="battery-good-charging"
     URGENCY="normal"
-    SOUND="power-plug"
+    SOUND="/run/current-system/sw/share/sounds/freedesktop/stereo/power-plug.oga"
   elif ((POWER_SUPPLY_ONLINE==0)); then
     TITLE="Disconnected"
     BODY="$current_percentage% of battery remaining."
     ICON="battery-caution"
     URGENCY="low"
-    SOUND="power-unplug"
+    SOUND="/run/current-system/sw/share/sounds/freedesktop/stereo/power-unplug.oga"
   else
     exit 0
   fi
@@ -34,9 +32,9 @@ pkgs.writeShellScript "batplug" ''
       "$BODY" \
       -i "$ICON" \
       -u "$URGENCY" \
-      -r "$NOTIFICATION_ID"
+      -h "string:synchronous:battery"
 
   ${pkgs.util-linux}/bin/runuser -u ${config.users.users.omareloui.name} -- \
-    env DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus" \
-    ${pkgs.libcanberra-gtk3}/bin/canberra-gtk-play -i "$SOUND"
+    env XDG_RUNTIME_DIR="/run/user/1000" \
+    ${pkgs.pulseaudio}/bin/paplay "$SOUND"
 ''

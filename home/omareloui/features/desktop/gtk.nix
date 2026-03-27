68870dac8740dc942d7bceff47522d3f5306cd66
Omar Eloui <contact@omareloui.com>
2026-03-27 18:33:21 +0200

    chore(gtk): fix a gtk warning

--- home/omareloui/features/desktop/gtk.nix
+++ home/omareloui/features/desktop/gtk.nix
@@ -1,5 +1,10 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   gtk.enable = true;
+  gtk.gtk4.theme = config.gtk.theme;
   services.xsettingsd.enable = true;
   xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
 }

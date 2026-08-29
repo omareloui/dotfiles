2d3fb1d920bccaeeb5790e3ab5d12b069dc4e653
Omar Eloui <contact@omareloui.com>
2026-08-29 10:54:54 +0300

    fix(wlogout): fix the logout command

--- home/omareloui/features/desktop/hypr/wlogout/default.nix
+++ home/omareloui/features/desktop/hypr/wlogout/default.nix
@@ -27,7 +27,7 @@
       }
       {
         label = "logout";
-        action = "hyprctl dispatch exit 0";
+        action = "hyprctl dispatch 'hl.dsp.exit()'";
         text = "Logout (e)";
         keybind = "e";
       }
